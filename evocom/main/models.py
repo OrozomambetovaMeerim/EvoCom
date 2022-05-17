c0c04f9df0ec91967c6e128995674904229605c4
OrozomambetovaMeerim <meerimorozomambetova@gmail.com>
2022-05-17 14:16:12 +0600

    added models

--- evocom/main/models.py
+++ evocom/main/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.forms import ImageField
 
-# Create your models here.
+
+class About_Us(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name ='О нас'
+        ordering = ['text']
+
+
+
+class Choose_Us(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name ='Почему выбирают нас'
+        ordering = ['title']
+
+
+
+class Our_Services(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name ='Наши сервисы'
+        ordering = ['title']
+
+
+
+class Services(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name ='Наши услуги'
+        ordering = ['text']
+
+
+
+class Partners(models.Model):
+    image = models.ImageField(upload_to='main', null=True, blank=True)
+
+
+    class Meta:
+        verbose_name ='Партнеры'
+        ordering = ['image']
+
+
+
+class Contacts(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.IntegerField()
+    email = models.CharField(max_length=150)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name ='Контакты'
+        ordering = ['name']
