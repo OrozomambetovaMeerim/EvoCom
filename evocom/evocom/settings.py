28af1e89b14b0e90b0d8097d26e3368172859b64
OrozomambetovaMeerim <meerimorozomambetova@gmail.com>
2022-05-18 17:52:11 +0600

    install django-heroku

--- evocom/evocom/settings.py
+++ evocom/evocom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@gfqv(@mikkbfidw3#+l$3!!7r)(6e+tnbj1(h9wvvs_(v%64#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'evocom01.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'evocom02.herokuapp.com']
 
 
 # Application definition
