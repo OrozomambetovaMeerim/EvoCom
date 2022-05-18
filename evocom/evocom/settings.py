403fddccaaf7365929e0101b839d77ddd99d48cf
OrozomambetovaMeerim <meerimorozomambetova@gmail.com>
2022-05-18 17:24:24 +0600

    heroku deploy

--- evocom/evocom/settings.py
+++ evocom/evocom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@gfqv(@mikkbfidw3#+l$3!!7r)(6e+tnbj1(h9wvvs_(v%64#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'evocom01.herokuapp.com']
 
