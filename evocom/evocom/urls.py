584e840058907723a2e8fb8cc961e666165e2824
OrozomambetovaMeerim <meerimorozomambetova@gmail.com>
2022-05-18 10:24:49 +0600

    added requirements.txt and serializers files

--- evocom/evocom/urls.py
+++ evocom/evocom/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import About_UsListAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', include('main.urls')),
+    path('', About_UsListAPIView.as_view(), name='about_us-list'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- evocom/main/views.py
+++ evocom/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import About_UsSerializer
+from .models import *
+
+
+class About_UsListAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        about_us = About_Us.objects.all()
+        about_us_json = About_UsSerializer(about_us, many=True)
+        return Response(data=about_us_json.data)
